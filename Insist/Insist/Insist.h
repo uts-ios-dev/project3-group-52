f58a3279439b71bde0b6897de30fc1239d36590e
Shiwei Lin <12433126@student.uts.edu.au>
2018-05-15 15:36:22 +1000

    v1
    Test version 1

---
+++ Insist/Insist/Insist.h
@@ -0,0 +1,19 @@
+//
+//  Insist.h
+//  Insist
+//
+//  Created by Shiwei Lin on 15/5/18.
+//  Copyright © 2018 Shiwei Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Insist.
+FOUNDATION_EXPORT double InsistVersionNumber;
+
+//! Project version string for Insist.
+FOUNDATION_EXPORT const unsigned char InsistVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Insist/PublicHeader.h>
+
+
