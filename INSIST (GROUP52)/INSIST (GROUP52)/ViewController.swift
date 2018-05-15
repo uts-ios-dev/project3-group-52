672eaa6016c99cecbf6225ed2f6af10a97d62f57
Shiwei Lin <12433126@student.uts.edu.au>
2018-05-15 20:54:42 +1000

    UI V1 by Renwei
    Co-Authored-By: alcorpisces <alcorpisces@users.noreply.github.com>

---
+++ INSIST (GROUP52)/INSIST (GROUP52)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  INSIST (GROUP52)
+//
+//  Created by Renwei Liu on 15/5/18.
+//  Copyright © 2018 Renwei Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
