599e9edd65c9c6ea2d997cc5b75ec9c43c9b75f0
Shiwei Lin <12433126@student.uts.edu.au>
2018-05-15 15:42:09 +1000

    V1
    Test v1

---
+++ Insist(Group 52)/Insist(Group 52)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Insist(Group 52)
+//
+//  Created by Shiwei Lin on 15/5/18.
+//  Copyright © 2018 Shiwei Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
