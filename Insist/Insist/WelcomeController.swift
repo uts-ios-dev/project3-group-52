306358fcf46040a4969d32a1df17650c564cf1ab
Shiwei Lin <12433126@student.uts.edu.au>
2018-05-16 23:40:36 +1000

    Finish FB login and change some UI

---
+++ Insist/Insist/WelcomeController.swift
@@ -0,0 +1,42 @@
+//
+//  WelcomeController.swift
+//  Insist
+//
+//  Created by Shiwei Lin on 16/5/18.
+//  Copyright © 2018 Shiwei Lin. All rights reserved.
+//
+
+import UIKit
+import FBSDKLoginKit
+
+class WelcomeController: UIViewController, FBSDKLoginButtonDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let loginButton = FBSDKLoginButton()
+        view.addSubview(loginButton)
+        loginButton.frame = CGRect(x: view.frame.width / 4, y: view.frame.height / 2 + 120, width: view.frame.width / 2, height: 50)
+        loginButton.delegate = self
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("Log out")
+    }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if error != nil{
+            print(error)
+            return
+        }
+        print("Logged in")
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
---
+++ Insist/Pods/Target Support Files/Pods-Insist/Pods-Insist-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Insist : NSObject
+@end
+@implementation PodsDummy_Pods_Insist
+@end
